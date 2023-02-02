c7b66f4b0ecaea973020e0f833745242906f7df6
Waris Radji <wradji64@gmail.com>
2023-02-02 02:22:23 +0100

    Change GPU device

--- src/net.py
+++ src/net.py
@@ -97,7 +97,7 @@ def train(
 
 
 if __name__ == "__main__":
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
     writer = SummaryWriter
     dataset = torch.load("dataset.pt")
     train_dataset = {}
